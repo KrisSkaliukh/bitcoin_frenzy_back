1a1ee3fa433b296ec3717a261cb398833c542d30
e.glazunova <e.glazunova@dunice.net>
2022-06-21 10:42:18 +0300

    Init DB

---
+++ migrations/20220621065522-create-user.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      count_money: {
+        type: Sequelize.INTEGER
+      },
+      count_bitcoins: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ models/bitcoin.js
@@ -0,0 +1,23 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Bitcoin extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Bitcoin.init({
+    bitcoin_price: DataTypes.NUMBER
+  }, {
+    sequelize,
+    modelName: 'Bitcoin',
+  });
+  return Bitcoin;
+};
